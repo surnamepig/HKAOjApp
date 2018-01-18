29330abfa143606467f8192025ec2919699582c9
tinkpad588 <tinkpad588@aliyun.com>
2018-01-18 16:56:38 +0800

    update

--- src/screen/AnnualPassIntroduce.js
+++ src/screen/AnnualPassIntroduce.js
@@ -1,12 +1,109 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    Dimensions,
+    ScrollView
+} from 'react-native';
+
+const width = Dimensions.get('window').width;
 
 export default class AnnualPassIntroduceScreen extends Component {
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>AnnualPassIntroduce Screen</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <View style={styles.content}>
+                    <Image resizeMode='contain' style={styles.image}
+                           source={require('../assets/images/annual_intro_top_cn.jpg')}/>
+                    <Text style={styles.text}>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        如果您需要经常来往亚洲各大城市，香港航空年票（Airpass）是您最佳的选择。只需成为金鹏俱乐部会员即可选购香港、台北、中国内地、东京、大阪及曼谷的3套或5套的往返年票产品，套数越多价格越优惠，还有经济舱和商务舱任您选择！免费更改航班日期，为您提供更多舒适、灵活的出行选择。
+                    </Text>
+                    <Text style={styles.text}>★ SkyTrax四星级优质服务</Text>
+                    <Text style={styles.text}>★ 弹性更改航班日期</Text>
+                    <Text style={styles.text}>★ 年票价格更优惠</Text>
+                    <Text style={styles.item}>立即抢购！</Text>
+                    <Text style={styles.listTitle}>年票使用手册</Text>
+                </View>
+                <View style={styles.options}>
+                    <Text style={styles.itemTitle}>1.进入年票预订，生成订单</Text>
+                    <Image resizeMode='contain' style={styles.itemImage}
+                           source={require('../assets/images/annual1.jpg')}/>
+                    <Text style={styles.itemTitle}>2.登录会员，查看年票订单</Text>
+                    <Image resizeMode='contain' style={styles.itemImage}
+                           source={require('../assets/images/annual2.jpg')}/>
+                    <Text style={styles.itemTitle}>3.进入年票订单详情，开启年票之旅</Text>
+                    <Image resizeMode='contain' style={styles.itemImage}
+                           source={require('../assets/images/annual3.jpg')}/>
+                </View>
+            </ScrollView>
         );
     }
 }
+
+var height = 96 * width / 64
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fcfcfc'
+    },
+    header: {
+        flexDirection: 'row-reverse',
+        height: 50,
+        textAlign: 'center',
+        width: width,
+        borderBottomWidth: 1,
+        borderColor: '#b7b6ba',
+        justifyContent: 'center'
+    },
+    headerTitle: {
+        fontSize: 20,
+        lineHeight: 50,
+        fontWeight: '600',
+    },
+    content: {
+        width: width
+    },
+    image: {
+        width: width,
+        height: 130
+    },
+    itemImage: {
+        width: '100%',
+        height: height
+    },
+    text: {
+        color: '#666',
+        padding: 10,
+        fontWeight: '600'
+    },
+    item: {
+        padding: 15,
+        color: '#666'
+    },
+    listTitle: {
+        fontSize: 22,
+        fontWeight: '600',
+        width: width,
+        textAlign: 'center',
+        height: 70
+    },
+    itemTitle: {
+        fontSize: 18,
+        marginBottom: 10,
+        fontWeight: '600'
+    },
+    options: {
+        width: width,
+        padding: 15,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginBottom: 80
+    },
+    option: {
+        width: '90%',
+        marginBottom: 180
+    }
+});
--- src/screen/AnnualPassTerms.js
+++ src/screen/AnnualPassTerms.js
@@ -1,12 +1,145 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    TouchableHighlight,
+    Image,
+    ScrollView
+} from 'react-native';
 
 export default class AnnualPassTermsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkState: false,
+            buttonClass:'button',
+            textClass: 'buttonText'
+        };
+    }
+
     render() {
+        let imgSource = this.state.checkState == true
+            ? require('../assets/images/checked.png') : require('../assets/images/check.png');
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>AnnualPassTerms Screen</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <View style={styles.content}>
+                    <Text style={styles.text}>1. 有效期: 订购日起一年内</Text>
+                    <Text style={styles.text}>2. 机票最短/最长停留期：当日往返/6个月</Text>
+                    <Text style={styles.text}>3. 改期: 免费</Text>
+                    <Text style={styles.text}>4. 退票: 全部未使用年票可退款，需收取HKD1200 </Text>
+                    <Text style={styles.text}>5. 误机: 旅客必须于航班起飞前最少2小时改期,变更或取消航班，否则每次将收取误机费HKD1200 </Text>
+                    <Text style={styles.text}>6. 舱位: 经济舱 L;商务舱 D </Text>
+                    <Text style={styles.text}>7.
+                        3张年票产品，最多可供指定的2位乘客使用，5张年票产品，最多可供指定的3位乘客使用；购买时须确定指定乘客姓名及其分配的张数，一旦订单确定，乘客人数不可增加/减少，姓名及其分配的机票张数不可更改</Text>
+                    <Text style={styles.text}>8. 票价可能会以交易当时之汇率而变更；</Text>
+                    <Text style={styles.text}>9.
+                        请浏览http://www.hongkongairlines.com/pdf/2018AirpassTermsAndConditionsSC.pdf参阅详细条款细则</Text>
+                </View>
+                <View style={styles.check}>
+                    <View style={styles.checkNav}>
+                        <TouchableWithoutFeedback style={styles.button}
+                                                  onPress={() => {
+                                                      this.setState({checkState: this.state.checkState == true ? false : true})
+                                                  }}
+                        >
+                            <Image style={styles.checkImg} resizeMode='contain'
+                                   source={imgSource}/>
+                        </TouchableWithoutFeedback>
+                        <Text style={styles.checkText}>*我已阅读并同意以上条款规则 </Text>
+                    </View>
+                    <View>
+                    <View style={styles.buttonNav}>
+                        <TouchableWithoutFeedback style={styles.buttonTouch}
+                                                  onPressIn={() => {
+                                                      this.setState({buttonClass:'buttonActive',textClass:'buttonTextActive'});
+                                                  }}
+                                                  onPressOut={() => {
+                                                      this.setState({buttonClass:'button',textClass:'buttonText'});
+                                                  }}
+                        >
+                            <View style={styles[this.state.buttonClass]}>
+                                <Text style={styles[this.state.textClass]}>确定</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    content: {
+        padding: 12
+    },
+    text: {
+        marginBottom: 16,
+        fontSize: 16,
+        color: '#333',
+        fontFamily: 'helvetica'
+    },
+    check: {
+        borderTopColor: '#b3b3b3',
+        borderTopWidth: 1
+    },
+    checkNav: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingLeft: 12
+    },
+    checkImg: {
+        width: 22,
+        height: 22
+    },
+    checkText: {
+        fontSize: 14,
+        color: '#333',
+        paddingLeft: 5
+    },
+    buttonNav: {
+        height: 180,
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonTouch:{
+        height: 50
+    },
+    button: {
+        height: 50,
+        width: '94%',
+        borderWidth: 1,
+        borderColor: '#f02828',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        backgroundColor: '#fff'
+    },
+    buttonActive:{
+        height: 50,
+        width: '94%',
+        borderWidth: 1,
+        borderColor: '#f02828',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        backgroundColor: '#f02828'
+    },
+    buttonText: {
+        color: '#f02828',
+        fontSize: 16
+    },
+    buttonTextActive: {
+        color: '#fff',
+        fontSize: 16
+    }
+});
